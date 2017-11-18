d57ac275f61b7bac0a3ecfc6bd38c4cca22d1a1a
TriiNoxYs <contact@triinoxys.fr>
2017-11-18 15:16:37 +0000

    Remove Stats in About command

--- lib/roi_des_poros/commands/about.rb
+++ lib/roi_des_poros/commands/about.rb
@@ -17,7 +17,7 @@ module RoiDesPoros
           e.description = "Je suis le **Roi des Poros**. Mon but est d'animer le serveur et de répondre à *tous* vos besoins!"
 
           e.add_field name: '__Infos:__', value: "**Développeur:** <@131218864234168321>  \n**Propriétaire de l'instance:** <@#{CONFIG.owner_id}>  \n**GitHub:** [github.com/TriiNoxYs/RoiDesPoros](https://github.com/TriiNoxYs/RoiDesPoros)  \n**Discord:** [discord.gg/sKYmk9c](https://discord.gg/sKYmk9c)  \n**Mail:** contact@triinoxys.fr", inline: true
-          e.add_field name: '__Stats:__', value: "Soon!", inline: true
+          # e.add_field name: '__Colonne 2:__', value: "blblbl", inline: true
 
           e.footer = { text: "Roi des Poros v#{VERSION} by TriiNoxYs" }
         end
