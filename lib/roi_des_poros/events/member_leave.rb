5ddf6a47b2afe0dfd7897db6cc286c76f507c30f
TriiNoxYs <contact@triinoxys.fr>
2017-11-20 23:13:15 +0000

    Fix name bug on member leave

--- lib/roi_des_poros/events/member_leave.rb
+++ lib/roi_des_poros/events/member_leave.rb
@@ -1,11 +1,11 @@
 module RoiDesPoros
   module Events
-    # Say welcome to new users
+    
     module LeaveMessage
       extend Discordrb::EventContainer
       member_leave do |event|
         unless event.server.bans.include? event.user
-          event.server.text_channels.select{ |channel| channel.id == CONFIG.welcome_channel }[0].send_message("Oh, <@#{event.member.id}> nous a quittés <:porosad:371719545762742304>")
+          event.server.text_channels.select{ |channel| channel.id == CONFIG.welcome_channel }[0].send_message("Oh, #{event.member.username}##{event.member.discriminator} nous a quittés <:porosad:371719545762742304>")
         end
       end
     end
