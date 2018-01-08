f56f7e6bf86afa17cd644113715f64b64c2a8b76
TriiNoxYs <contact@triinoxys.fr>
2018-01-08 12:13:24 -0500

    Rework Jeu command

--- lib/roi_des_poros/commands/jeu.rb
+++ lib/roi_des_poros/commands/jeu.rb
@@ -2,54 +2,62 @@ module RoiDesPoros
 
   module Commands
 
-    #
     module Jeu
       extend Discordrb::Commands::CommandContainer
-      
-      games = [      
-          fortnite = [:"fortnite"],
-          league_of_legends = [:"league_of_legends", :"lol", :"league_of_legend", :"league of legends", :"league of legend"],
-          minecraft = [:"minecraft", :"mc"],
-          payday2 = [:"payday2", :"payday_2", :"payday 2"],
-          call_of_duty = [:"call_of_duty",:"call of duty", :"cod", :"callofduty"],
-          csgo = [:"csgo", :"cs_go", :"cs go", :"counter_strike", :"counter strike", :"counterstrike"],
-          fifa = [:"fifa"],
-          paladins = [:"paladins", :"paladin"],
-          rocket_league = [:"rocket_league",:"rocket league", :"rocketleague"],
-          dofus = [:"dofus"]
-      ]
-      
-      roles = {
-          :fortnite          => 373807478900523013,
-          :league_of_legends => 373807585415135253,
-          :minecraft         => 373811887562948609,
-          :payday2           => 373814297626673152,
-          :call_of_duty      => 381454112081838082,
-          :csgo              => 373811989614690315,
-          :fifa              => 373942275572498447,
-          :paladins          => 373820014178074626,
-          :rocket_league     => 373819423665946624,
-          :dofus             => 373819199388385281,
+
+# Roles format
+=begin
+      games = {
+	  :fortnite  => {role_id: "373807478900523013", names: ["fortnite"]},
+          :minecraft => {role_id: "373811887562948609", names: ["minecraft, mc"]}
       }
+=end
       
-      
-      command(:jeu, description: 'Attribue un rôle en fonction du jeu souhaité.', min_args: 1) do |event, role|
-      
-        role = role.downcase.to_sym
-        success = false
+      command(:jeu, description: 'Système de jeux pour trouver des potos avec qui jouer.', usage: '!jeu <nom du jeu>') do |event, *args|
+	file = File.read('/opt/ruby/discord/apps/RoiDesPoros/data/games.json')
+        games = JSON.parse(file)
         
-        games.each do |game|
-            if game.include? role
-                event.user.add_role(roles[game[0]])
+        # List
+	if args.size == 0
+	  response = "Voici la liste des jeux, Chef! <:ouichef:374627649370324992>\n```"
+	  games.each do |game| 
+	    response << " - #{game[0]}"
+	    if game[1]["names"].size > 1 then response << " (alias: #{game[1]["names"].drop(1).join(', ')})" end
+	    response << "\n"
+	  end
+	  response << "```"
+	  event.respond(response)
+	else
+	  # Create
+	  if args[0].downcase == "create"
+	    if event.user.id != 131218864234168321 then event.respond("Tu ne peux pas faire ça!") && return end
+	    games[args[1]] = {role_id: args[2], names: [args[1].downcase]}
+	    File.open("/opt/ruby/discord/apps/RoiDesPoros/data/games.json", "w") { |f| f.write(JSON.pretty_generate(games)) }
+	    event.respond("Le jeu a bien été créé! <:porocoolguy:371719520559300608>")
+
+	  # Delete	  
+	  elsif args[0].downcase == "delete"
+            if event.user.id != 131218864234168321 then event.respond("Tu ne peux pas faire ça!") && return end	  
+	    games.delete(args[1])
+	    File.open("/opt/ruby/discord/apps/RoiDesPoros/data/games.json", "w") { |f| f.write(JSON.pretty_generate(games)) }
+	    event.respond("Le jeu a bien été supprimé! <:porosad:371719545762742304>")	  	
+
+	  # Join
+	  else
+            success = false
+            games.each do |game|
+	      if game[1]["names"].include? args[0].downcase
+                event.user.add_role(game[1]["role_id"])
                 success = true
                 break
+              end
             end
-        end
-        
-        if success then event.respond("Oui, Chef! Voila, Chef! <:ouichef:374627649370324992>")
-        else event.respond("Euh Chef... Je ne trouve pas votre jeu... <:porosad:371719545762742304>")
-        end
-        
+
+            if success then event.respond("Oui, Chef! Voila, Chef! <:ouichef:374627649370324992>")
+            else event.respond("Euh Chef... Je ne trouve pas votre jeu... <:porosad:371719545762742304>")
+            end
+	  end  
+	end
       end
     end
   end
