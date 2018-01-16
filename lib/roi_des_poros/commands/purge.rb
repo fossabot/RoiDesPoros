211b66c7f0c9e183f035f29814149c5e73bcd2ab
TriiNoxYs <contact@triinoxys.fr>
2018-01-16 18:33:48 -0500

    Add alias to purge command

--- lib/roi_des_poros/commands/purge.rb
+++ lib/roi_des_poros/commands/purge.rb
@@ -5,12 +5,7 @@ module RoiDesPoros
     # Prints most useful information about the bot.
     module Purge
       extend Discordrb::Commands::CommandContainer
-      command(:purge, description: 'Efface les N derniers messages.', min_args: 1, max_args: 1, required_permissions: [:manage_messages]) do |event, n|
-          if n.to_i.to_s == n then event.channel.prune(n.to_i + 1)
-          else event.respond(":x: Merci d'entrer un nombre de messages à supprimer.") end
-      end
-      
-      command(:p, description: 'Alias pour la commande purge.', min_args: 1, max_args: 1, required_permissions: [:manage_messages]) do |event, n|
+      command([:purge, :p], description: 'Efface les N derniers messages.', min_args: 1, max_args: 1, required_permissions: [:manage_messages]) do |event, n|
           if n.to_i.to_s == n then event.channel.prune(n.to_i + 1)
           else event.respond(":x: Merci d'entrer un nombre de messages à supprimer.") end
 	  return
