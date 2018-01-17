27678400e120cc0e3ff61d5c17ac0c796858489b
TriiNoxYs <contact@triinoxys.fr>
2018-01-17 14:11:21 -0500

    Add username under emoji and deal with 404 errors

--- lib/roi_des_poros/commands/emoji.rb
+++ lib/roi_des_poros/commands/emoji.rb
@@ -6,19 +6,28 @@ module RoiDesPoros
       extend Discordrb::Commands::CommandContainer
 
       command(:emoji, description: 'Affiche un emoji en grand.', usage: '!emoji <emoji>', min_args: 1, max_args: 1) do |event, emoji|
-	event.message.delete
-
 	if !event.message.emoji?
 	  code = Twemoji.find_by_text(Twemoji.find_by(unicode: emoji))
-	  file = "data/emojis/#{code}.png"
 	  url = "https://twemoji.maxcdn.com/2/72x72/#{code}.png"
 	else
-	  file = "data/emojis/#{event.message.emoji[0].name}.png"
+	  code = event.message.emoji[0].name
    	  url = event.message.emoji[0].icon_url
 	end
 
-        File.write(file, open(url).read) unless File.file? file
-        event.channel.send_file(File.new(file))
+        file = "data/emojis/#{code}.png"
+
+        #File.write(file, open(url).read) unless File.file? file
+        #event.channel.send_file(File.new(file))
+
+        if code
+	  event.channel.send_embed do |embed|
+            embed.image = Discordrb::Webhooks::EmbedImage.new(url: url)
+            embed.footer = {text: "by #{event.user.name}" }
+          end
+	  event.message.delete
+	else
+	  event.respond("Cet emoji n'est pas (encore) possible à agrandir <:porocry:371729366981410829>")
+	end
 
 	return
       end
