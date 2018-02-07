a31d6bd439b159d13d01d4ffdf81b957c29e7c26
TriiNoxYs <contact@triinoxys.fr>
2018-02-07 13:22:35 -0500

    Add --noby option to Emoji command

--- lib/roi_des_poros/commands/emoji.rb
+++ lib/roi_des_poros/commands/emoji.rb
@@ -5,9 +5,9 @@ module RoiDesPoros
     module Emoji
       extend Discordrb::Commands::CommandContainer
 
-      command(:emoji, description: 'Affiche un emoji en grand.', usage: '!emoji <emoji>', min_args: 1, max_args: 1) do |event, emoji|
+      command(:emoji, description: 'Affiche un emoji en grand.', usage: '!emoji <emoji>', min_args: 1, max_args: 2) do |event, *args|
 	if !event.message.emoji?
-	  code = Twemoji.find_by_text(Twemoji.find_by(unicode: emoji))
+	  code = Twemoji.find_by_text(Twemoji.find_by(unicode: args[0]))
 	  url = "https://twemoji.maxcdn.com/2/72x72/#{code}.png"
 	else
 	  code = event.message.emoji[0].name
@@ -16,17 +16,24 @@ module RoiDesPoros
 
         file = "data/emojis/#{code}.png"
 
-        #File.write(file, open(url).read) unless File.file? file
-        #event.channel.send_file(File.new(file))
-
-        if code
-	  event.channel.send_embed do |embed|
-            embed.image = Discordrb::Webhooks::EmbedImage.new(url: url)
-            embed.footer = {text: "by #{event.user.name}" }
+	if args.size >= 2 && args[1] == "--noby"
+	  if code
+            File.write(file, open(url).read) unless File.file? file
+            event.channel.send_file(File.new(file))
+            event.message.delete
+          else
+            event.respond("Cet emoji n'est pas (encore) possible à agrandir <:porocry:371729366981410829>")
           end
-	  event.message.delete
 	else
-	  event.respond("Cet emoji n'est pas (encore) possible à agrandir <:porocry:371729366981410829>")
+  	  if code
+	    event.channel.send_embed do |embed|
+              embed.image = Discordrb::Webhooks::EmbedImage.new(url: url)
+              embed.footer = {text: "by #{event.user.name}" }
+            end
+	    event.message.delete
+	  else
+	    event.respond("Cet emoji n'est pas (encore) possible à agrandir <:porocry:371729366981410829>")
+	  end
 	end
 
 	return
