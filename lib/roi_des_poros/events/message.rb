56dfe72dae98a11d0d5166bd2811ae1997b80ec4
TriiNoxYs <contact@triinoxys.fr>
2018-03-14 23:25:17 +0100

    Fix AntiJul duplic and add it to all channels

--- lib/roi_des_poros/events/message.rb
+++ lib/roi_des_poros/events/message.rb
@@ -1,26 +1,18 @@
 module RoiDesPoros
   module Events
+
     # For a better life...
     module AntiJul
       extend Discordrb::EventContainer
       message(starting_with: not!(CONFIG.prefix)) do |event|
         unless event.from_bot?
-          if (event.channel.name == "poro-zic" || event.channel.id == 372545764720640000) && (event.message.to_s.downcase.include?("jul"))
+#          if (event.channel.name == "poro-zic" || event.channel.id == 372545764720640000) && (event.message.to_s.downcase.include?("jul"))
+	   if event.message.to_s.downcase.include?("jul")
             event.respond("Attends attends... T'as dit quoi là ?  \nJul?! Ca vaut un ban ça tu sais...")
           end
         end
       end
     end
     
-    module AntiJul
-      extend Discordrb::EventContainer
-      message(starting_with: not!(CONFIG.prefix)) do |event|
-        unless event.from_bot?
-          if (event.channel.name == "poro-zic" || event.channel.id == 372545764720640000) && (event.message.to_s.downcase.include?("jul"))
-            event.respond("Attends attends... T'as dit quoi là ?  \nJul?! Ca vaut un ban ça tu sais...")
-          end
-        end
-      end
-    end
   end
 end
